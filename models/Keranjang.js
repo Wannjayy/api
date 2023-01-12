8bfd393b5d7a82c12bc45129f9319a5392ea000d
Ahmad Wahana Jaya <119087167+Wannjayy@users.noreply.github.com>
2023-01-13 03:00:16 +0700

    k

---
+++ models/Keranjang.js
@@ -0,0 +1,20 @@
+// (4) Buat Schema Product
+const mongoose = require('mongoose')
+
+const KeranjangSchema = mongoose.Schema({
+    // Buat Schema data
+    jumlah_pesanan: {
+        type: Number,
+        required: true
+    },
+    keterangan: {
+        type: String,
+        required: true
+    },
+    produk: {
+        type: Object,
+        required: true
+    },
+})
+
+module.exports = mongoose.model('Keranjang', KeranjangSchema)
---
+++ routes/keranjang.js
@@ -0,0 +1,45 @@
+// (5) Buat router Mahasiswa
+const express = require('express')
+const router = express.Router() 
+const Keranjang = require('../models/Keranjang')
+
+// Create 
+router.post('/', async(req, res) => {
+    // tampung input 
+    const keranjangPost = new Keranjang({
+        jumlah_pesanan: req.body.jumlah_pesanan,
+        keterangan: req.body.keterangan,
+        produk: req.body.produk
+    })
+
+    try {
+        // simpan data 
+        const keranjang = await keranjangPost.save()
+        // response
+        res.json(keranjang)
+    } catch (error) {
+        res.json({message: error})
+    }
+})
+// Read
+router.get('/', async(req, res) => {
+    try {
+        const keranjang = await Keranjang.find()
+        res.json(keranjang)
+    } catch (error) {
+        res.json({message: error})
+    }
+})
+// Delete 
+router.delete('/:keranjangId', async(req, res) => {
+    try {
+        // delete data 
+        const keranjang = await Keranjang.deleteOne({_id: req.params.keranjangId})
+        // response
+        res.json(keranjang)
+    } catch (error) {
+        res.json({message: error})
+    }
+})
+
+module.exports = router
