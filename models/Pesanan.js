b531e492cb6daecebce52ff8ad66cde1e0ef451b
Ahmad Wahana Jaya <119087167+Wannjayy@users.noreply.github.com>
2023-01-14 04:08:59 +0700

    Update Pesanan.js

--- models/Pesanan.js
+++ models/Pesanan.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const PesananSchema = mongoose.Schema({
     // Buat Schema data
     nama: {
-        type: Number,
+        type: String,
         required: true
     },
     keranjangs: {
